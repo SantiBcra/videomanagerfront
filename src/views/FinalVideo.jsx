e64d906f02cc6d619647197a1e94573bec910455
Santiago Becerra <123743418+SantiBcra@users.noreply.github.com>
2024-10-31 18:16:37 -0300

    Update FinalVideo.jsx

--- src/views/FinalVideo.jsx
+++ src/views/FinalVideo.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
+import './js/new'
+import './js/share_manager'
 import './css/perfect-scrollbar.css';
 import './css/mvp.css';
 
