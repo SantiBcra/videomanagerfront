74abbc67c353c3ba6338eec74e85d571f623a34d
Santiago Becerra <123743418+SantiBcra@users.noreply.github.com>
2024-11-06 11:37:29 -0300

    Update FinalVideo.jsx

--- src/views/FinalVideo.jsx
+++ src/views/FinalVideo.jsx
@@ -46,7 +46,7 @@ const VideoPlayer = () => {
           instanceName: "player1",
           activePlaylist: ".playlist-video:first-child",
           playerRatio: 2,
-          aspectRatio: 1, // Relación 9:16
+          aspectRatio: 0.5, // Relación 9:16
           activeItem: 0,
           volume: 0.5,
           autoPlay: true,
