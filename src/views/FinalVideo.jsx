71ca6aba9e22af8fd1270d536b8ee2841313ac8f
Santiago Becerra <123743418+SantiBcra@users.noreply.github.com>
2024-11-07 17:28:21 -0300

    Update FinalVideo.jsx

--- src/views/FinalVideo.jsx
+++ src/views/FinalVideo.jsx
@@ -77,14 +77,18 @@ const VideoPlayer = () => {
   
      <>
   <div className="playlist-video">
-         <div className="instagram-button-container">
-      <a href={`https://www.instagram.com/${instagram}`} target="_blank" rel="noopener noreferrer">
+    
+<div class="mvp-annotation-section">      
+
+     <div id="an1" class="mvp-annotation" data-show data-hide>
+          <a href={`https://www.instagram.com/${instagram}`} target="_blank" rel="noopener noreferrer">
         <img src={ig} alt="Ver en Instagram" style={{ width: "50px", height: "50px", borderRadius: "13px" }} />
       </a>
       <a href={`https://verisart.com/works/${nombre}`} target="_blank" rel="noopener noreferrer">
         <img src={ver} alt="Verisart" style={{ width: "50px", height: "50px", borderRadius: "13px" }} />
       </a>
-    </div>
+        </div> 
+  </div>
   
 
     <div id="wrapper">
