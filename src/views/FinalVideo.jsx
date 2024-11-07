65302970da7209c75fcbeaa8fc28f2167f16aa2f
Santiago Becerra <123743418+SantiBcra@users.noreply.github.com>
2024-11-07 17:50:21 -0300

    Update FinalVideo.jsx

--- src/views/FinalVideo.jsx
+++ src/views/FinalVideo.jsx
@@ -103,6 +103,47 @@ const VideoPlayer = () => {
           data-title="Video title goes here" 
           data-description="Self hosted video. Commodo vitae, tempor eu, urna eu mi hendrerit. Maecenas eu erat condimentum."
         >
+
+
+
+
+
+
+
+
+
+
+
+<div class="mvp-annotation-section">
+        
+        <div id="an1" class="mvp-annotation" data-show data-hide>
+            <span>This is a popup text visible through the whole video. Lorem ipsum dolor sit amet. Link <a href="http://www.google.com" target="_blank">here</a></span>
+            
+        </div> 
+
+        <div id="an2" class="mvp-annotation" data-show="5" data-hide="15">
+            <div class="an2-wrap">
+                <a href="http://www.google.com" target="_blank"><img src="data/ad-placeholder.jpg" alt=""/></a>
+                <div class="an2-title">Unlimited space</div>
+                <div class="an2-desc">Your advertizing space goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit...</div> 
+            </div> 
+            <div class="mvp-annotation-close" title="Close">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M193.94 256L296.5 153.44l21.15-21.15c3.12-3.12 3.12-8.19 0-11.31l-22.63-22.63c-3.12-3.12-8.19-3.12-11.31 0L160 222.06 36.29 98.34c-3.12-3.12-8.19-3.12-11.31 0L2.34 120.97c-3.12 3.12-3.12 8.19 0 11.31L126.06 256 2.34 379.71c-3.12 3.12-3.12 8.19 0 11.31l22.63 22.63c3.12 3.12 8.19 3.12 11.31 0L160 289.94 262.56 392.5l21.15 21.15c3.12 3.12 8.19 3.12 11.31 0l22.63-22.63c3.12-3.12 3.12-8.19 0-11.31L193.94 256z"></path></svg>
+            </div>
+        </div>   
+
+        <div id="an3" class="mvp-annotation" data-show="20" data-hide="30">
+            <a href="http://www.google.com" target="_blank"><span>This popup is a link and cannot be closed.</span></a>
+        </div>
+
+    </div>
+
+
+
+
+
+
+          
           <div className="mvp-subtitles">
             <div data-label="English" data-src={sub} data-default></div>
 {/*             <div data-label="Spanish" data-src="/subtitles/sintel-es.vtt"></div> */}
