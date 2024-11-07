6ac1a3cc7a638eef92e2b768cf3b88f5b46caf27
Santiago Becerra <123743418+SantiBcra@users.noreply.github.com>
2024-11-07 18:13:32 -0300

    Update FinalVideo.jsx

--- src/views/FinalVideo.jsx
+++ src/views/FinalVideo.jsx
@@ -114,18 +114,18 @@ const VideoPlayer = () => {
             
         </div> 
 
-        {/* <div id="an2" class="mvp-annotation" data-show="3" data-hide="6">
-          <a href={`https://www.instagram.com/${instagram}`} target="_blank" rel="noopener noreferrer">
-        <img src={ig} alt="Ver en Instagram" style={{ width: "50px", height: "50px", borderRadius: "13px" }} />
-      </a>
-      <a href={`https://verisart.com/works/${nombre}`} target="_blank" rel="noopener noreferrer">
-        <img src={ver} alt="Verisart" style={{ width: "50px", height: "50px", borderRadius: "13px" }} />
-      </a>
-        </div>   */}
+           
 
-        <div class="mvp-popup" data-show="pause">
+        <div class="mvp-popup" data-show="5">
 
-        <img class="mvp-popup-img" src={ig} alt=""/>
+       <div>
+              <a href={`https://www.instagram.com/${instagram}`} target="_blank" rel="noopener noreferrer">
+            <img src={ig} alt="Ver en Instagram" style={{ width: "50px", height: "50px", borderRadius: "13px" }} />
+          </a>
+          <a href={`https://verisart.com/works/${nombre}`} target="_blank" rel="noopener noreferrer">
+            <img src={ver} alt="Verisart" style={{ width: "50px", height: "50px", borderRadius: "13px" }} />
+          </a>
+      </div> 
 
       
 
