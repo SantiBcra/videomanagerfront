539362c21f06e9b9101ba7d2ad38a04324083dde
Santiago Becerra <123743418+SantiBcra@users.noreply.github.com>
2024-11-08 08:25:32 -0300

    Update FinalVideo.jsx

--- src/views/FinalVideo.jsx
+++ src/views/FinalVideo.jsx
@@ -82,6 +82,8 @@ const VideoPlayer = () => {
 
        
   <div className="playlist-video">
+
+    
     
     <div id="wrapper">
 
@@ -98,7 +100,14 @@ const VideoPlayer = () => {
         >
 
 
-
+ <div>
+              <a href={`https://www.instagram.com/${instagram}`} target="_blank" rel="noopener noreferrer">
+            <img src={ig} alt="Ver en Instagram" style={{ width: "50px", height: "50px", borderRadius: "13px" }} />
+          </a>
+          <a href={`https://verisart.com/works/${nombre}`} target="_blank" rel="noopener noreferrer">
+            <img src={ver} alt="Verisart" style={{ width: "50px", height: "50px", borderRadius: "13px" }} />
+          </a>
+      </div> 
 
 
 <button type="button" class="mvp-unmute-toggle mvp-btn-reset" aria-label="Unmute"></button>
@@ -108,22 +117,12 @@ const VideoPlayer = () => {
 
 
 <div class="mvp-annotation-section">
+  
+ <div id="an3" class="mvp-annotation" data-show="20" data-hide="60">
+        <a href="http://www.google.com" target="_blank"><span>This popup is a link and cannot be closed.</span></a>
+    </div>
 
-
-        <div class="mvp-popup" data-show="5">
-
-       <div>
-              <a href={`https://www.instagram.com/${instagram}`} target="_blank" rel="noopener noreferrer">
-            <img src={ig} alt="Ver en Instagram" style={{ width: "50px", height: "50px", borderRadius: "13px" }} />
-          </a>
-          <a href={`https://verisart.com/works/${nombre}`} target="_blank" rel="noopener noreferrer">
-            <img src={ver} alt="Verisart" style={{ width: "50px", height: "50px", borderRadius: "13px" }} />
-          </a>
-      </div> 
-
-      
-
-        </div> 
+       
 
 
 </div>
