d2c014569808b487a3f8e44752ed2c3a62761058
Santiago Becerra <123743418+SantiBcra@users.noreply.github.com>
2024-11-08 11:18:42 -0300

    Update FinalVideo.jsx

--- src/views/FinalVideo.jsx
+++ src/views/FinalVideo.jsx
@@ -34,7 +34,7 @@ const VideoPlayer = () => {
           gDriveAppId: "AIzaSyDMCGHB4N2fix7tvzGLFyRJjUZ2gHlQCrk",
           preload: 'auto',
           useMobileNativePlayer: false,
-          skin: "pollux",
+          skin: "sirius",
           mobileSeekbarMinWidth: 500,
           embedSrc: "0",
           openFsOnPlay: false,
