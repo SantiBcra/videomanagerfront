ae0dec719281a99b023cb8f5e259feebc6b7938c
Santiago Becerra <123743418+SantiBcra@users.noreply.github.com>
2024-11-08 10:30:00 -0300

    Update FinalVideo.jsx

--- src/views/FinalVideo.jsx
+++ src/views/FinalVideo.jsx
@@ -85,13 +85,13 @@ const VideoPlayer = () => {
 
 <div>
               <a href={`https://www.instagram.com/${instagram}`} target="_blank" rel="noopener noreferrer">
-            <img src={ig} alt="Ver en Instagram" style={{ width: "50px", height: "50px", borderRadius: "13px" }} />
+            <img src={ig} alt="Ver en Instagram" style="width: 50px; height: 50px; border-radius: 13px; margin: 0 auto; display: block;" />
           </a>
          
       </div> 
     <div id="wrapper">
 
-<div> <h2>Video is loading, please wait</h2></div>
+
         <div 
           className="mvp-playlist-item" 
           data-type="gdrive_folder" 
@@ -120,11 +120,10 @@ const VideoPlayer = () => {
      
         
 <div>
-            
-          <a href={`https://verisart.com/works/${nombre}`} target="_blank" rel="noopener noreferrer">
-            <img src={ver} alt="Verisart" style={{ width: "50px", height: "50px", borderRadius: "13px" }} />
-          </a>
-      </div> 
+  <a href={`https://verisart.com/works/${nombre}`} target="_blank" rel="noopener noreferrer">
+    <img src={ver} alt="Verisart" style="width: 50px; height: 50px; border-radius: 13px; margin: 0 auto; display: block;" />
+  </a>
+</div>
        
     </div>
 
