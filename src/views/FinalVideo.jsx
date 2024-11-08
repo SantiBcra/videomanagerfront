72036ff1a631f49b605481602e7d1deba3bec5fc
Santiago Becerra <123743418+SantiBcra@users.noreply.github.com>
2024-11-08 11:14:58 -0300

    Update FinalVideo.jsx

--- src/views/FinalVideo.jsx
+++ src/views/FinalVideo.jsx
@@ -32,7 +32,7 @@ const VideoPlayer = () => {
         // Configura las opciones del reproductor usando videoData.videoPath
         const settings = {
           gDriveAppId: "AIzaSyDMCGHB4N2fix7tvzGLFyRJjUZ2gHlQCrk",
-          preload: 'auto'
+          preload: 'auto',
           useMobileNativePlayer: false,
           skin: "pollux",
           mobileSeekbarMinWidth: 500,
