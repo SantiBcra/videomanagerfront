cd0bfb9006d3004b7edf9ac39a01ef298cddca2e
Santiago Becerra <123743418+SantiBcra@users.noreply.github.com>
2024-11-11 08:56:42 -0300

    Update FinalVideo.jsx

--- src/views/FinalVideo.jsx
+++ src/views/FinalVideo.jsx
@@ -35,9 +35,9 @@ const VideoPlayer = () => {
         const settings = {
           gDriveAppId: "AIzaSyDMCGHB4N2fix7tvzGLFyRJjUZ2gHlQCrk",
           preload: 'auto',
-          useMobileListMenu: true,
           useMobileNativePlayer: false,
           // skin: "aviva",
+          useBlob:true,
           mobileSeekbarMinWidth: 500,
           embedSrc: "0",
           openFsOnPlay: true,
@@ -110,6 +110,8 @@ const VideoPlayer = () => {
           data-title="Video title goes here" 
           data-description="Self hosted video. Commodo vitae, tempor eu, urna eu mi hendrerit. Maecenas eu erat condimentum."
         >
+
+          <a href="https://google.com" target="_blank" data-tooltip="I am a custom button" class="mvp-contr-btn mvp-btn-reset my-custom-button"><svg viewBox="0 0 512 512"><path d="M336 352c97.2 0 176-78.8 176-176S433.2 0 336 0S160 78.8 160 176c0 18.7 2.9 36.8 8.3 53.7L7 391c-4.5 4.5-7 10.6-7 17v80c0 13.3 10.7 24 24 24h80c13.3 0 24-10.7 24-24V448h40c13.3 0 24-10.7 24-24V384h40c6.4 0 12.5-2.5 17-7l33.3-33.3c16.9 5.4 35 8.3 53.7 8.3zM376 96a40 40 0 1 1 0 80 40 40 0 1 1 0-80z"/></svg></a>
 
 
           <div className="mvp-subtitles">
