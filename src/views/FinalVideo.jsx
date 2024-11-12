2a9c1a4bcd7515129bce592c627623894dd93e21
Santiago Becerra <123743418+SantiBcra@users.noreply.github.com>
2024-11-12 18:43:38 -0300

    Update FinalVideo.jsx

--- src/views/FinalVideo.jsx
+++ src/views/FinalVideo.jsx
@@ -59,12 +59,12 @@ const VideoPlayer = () => {
           randomPlay: false,
           showControlsBeforeStart: true,
         buttonsTop:[
-    {
-    full: `<a href="{https://verisart.com/works/${nombre}" target="_blank" data-tooltip="Certificate" class="mvp-contr-btn mvp-btn-reset my-custom-button"><svg viewBox="0 0 512 512"><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="400" font-family="Arial" font-weight="bold">V</text></svg></a>`,    
-    },
-    
+          {
+          full: `<a href="{https://verisart.com/works/${nombre}" target="_blank" data-tooltip="Certificate" class="mvp-contr-btn mvp-btn-reset my-custom-button"><svg viewBox="0 0 512 512"><text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="400" font-family="Arial" font-weight="bold">V</text></svg></a>`,    
+          },
+         { 
         icon: '<svg viewBox="0 0 448 512"><path d="M224,202.66A53.34,53.34,0,1,0,277.34,256,53.38,53.38,0,0,0,224,202.66Zm124.71-41a54,54,0,0,0-30.39-30.39C278.58,117,224.45,117,199.68,117h-1.36c-24.77,0-78.9,0-118.66,14.3A54,54,0,0,0,49.26,161.29C35,201.05,35,255.18,35,279.95v1.36c0,24.77,0,78.9,14.3,118.66a54,54,0,0,0,30.39,30.39c39.76,14.3,93.89,14.3,118.66,14.3h1.36c24.77,0,78.9,0,118.66-14.3a54,54,0,0,0,30.39-30.39C413,359.21,413,305.08,413,280.31v-1.36C413,255.18,413,201.05,348.71,161.29ZM224,338a82,82,0,1,1,82-82A82,82,0,0,1,224,338Zm85.6-134.4a19.2,19.2,0,1,1,19.2-19.2A19.21,19.21,0,0,1,309.6,203.6ZM398.8,314.61c-11.58,39.08-40.74,68.24-79.82,79.82-36.61,12.5-105.19,12.5-141.8,0-39.08-11.58-68.24-40.74-79.82-79.82-12.5-36.61-12.5-105.19,0-141.8C128.75,133.73,157.91,104.57,197,93c36.61-12.5,105.19-12.5,141.8,0,39.08,11.58,68.24,40.74,79.82,79.82,12.5,36.61,12.5,105.19,0,141.8Z" /></svg>',
- url: `https://www.instagram.com/${instagram}`,
+        url: `https://www.instagram.com/${instagram}`,
         target: "_blank",
         rel: "nofollow",
         class: 'my-other-button',
