b68cd3122298a76736929638d6a29992efbe85c3
Santiago Becerra <123743418+SantiBcra@users.noreply.github.com>
2024-11-13 10:02:56 -0300

    Update FinalVideo.jsx

--- src/views/FinalVideo.jsx
+++ src/views/FinalVideo.jsx
@@ -99,16 +99,7 @@ const VideoPlayer = () => {
 
 
        
-  <div className="playlist-video">
 
-{/* <div style={{ display: "flex", justifyContent: "center"}}>
-          <a href={`https://www.instagram.com/${instagram}`} target="_blank" rel="noopener noreferrer">
-                <img src={ig} alt="Ver en Instagram" style={{ width: "50px", height: "50px", borderRadius: "13px" }} />
-          </a>
-           <a href={`https://verisart.com/works/${nombre}`} target="_blank" rel="noopener noreferrer">
-                <img src={ver} alt="Verisart" style={{ width: "50px", height: "50px", borderRadius: "13px" }} />
-           </a>
-</div>  */}
     
     <div id="wrapper">
 
@@ -139,7 +130,7 @@ const VideoPlayer = () => {
      
 
        
-    </div>
+   
 
  
 
