fb39e0850fc278722570841732f2ad081e1ee1d4
Santiago Becerra <123743418+SantiBcra@users.noreply.github.com>
2024-11-13 10:43:13 -0300

    Update FinalVideo.jsx

--- src/views/FinalVideo.jsx
+++ src/views/FinalVideo.jsx
@@ -49,7 +49,7 @@ const VideoPlayer = () => {
           playlistBottomHeight: 0,
           mediaEndAction:'rewind',
           instanceName: "player1",
-          activePlaylist: ".playlist-video:first-child",
+          activePlaylist: "",
          playerRatio: 0.5,
           aspectRatio: 2, // Relación 9:16
           activeItem: 0,
@@ -103,22 +103,12 @@ const VideoPlayer = () => {
 
 
        
-  <div className="playlist-video">
 
-{/* <div style={{ display: "flex", justifyContent: "center"}}>
-          <a href={`https://www.instagram.com/${instagram}`} target="_blank" rel="noopener noreferrer">
-                <img src={ig} alt="Ver en Instagram" style={{ width: "50px", height: "50px", borderRadius: "13px" }} />
-          </a>
-           <a href={`https://verisart.com/works/${nombre}`} target="_blank" rel="noopener noreferrer">
-                <img src={ver} alt="Verisart" style={{ width: "50px", height: "50px", borderRadius: "13px" }} />
-           </a>
-</div>  */}
-    
     <div id="wrapper">
-
+ <div class="playlist-video">
 
         <div 
-          className="mvp-playlist-item" 
+         lass="mvp-playlist-item"
           data-type="gdrive_folder" 
           data-path={videoPath} // Asigna videoPath aquí 
           data-download="media/video/01.mp4" 
@@ -142,8 +132,8 @@ const VideoPlayer = () => {
         </div>
      
 
-       
-    </div>
+     </div>  
+    
 
  
 
